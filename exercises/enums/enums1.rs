2abcdba79bc595d6b4bd385aceaf9e2b70c5db05
Asaf Chen <asafc@asaf-chen.il.nextsilicon.com>
2022-12-20 16:54:19 +0200

    Finished enums, move_semantics5&5, structs, fixed a bug on variables4

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,15 +1,14 @@
 // enums1.rs
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define a few types of messages as used below
+    Quit, Echo, Move, ChangeColor,
 }
 
 fn main() {
-    println!("{:?}", Message::Quit);
+    let a = Message::Quit;
+    println!("{:?}", a);
     println!("{:?}", Message::Echo);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::ChangeColor);
--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,11 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Quit,
+    Move(Point),
 }
 
 struct Point {
@@ -37,7 +38,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(c) => self.change_color(c),
+            Message::Quit => self.quit(),
+            Message::Echo(echo_str) => self.echo(echo_str),
+            Message::Move(point) => self.move_position(point),
+        }
     }
 }
 
